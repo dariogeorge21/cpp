29d7bdb0f371c988be7fe3cb5b06ac5733b2d878
Dario George <edu.dariogeorge21@gmail.com>
2025-05-31 16:17:40 +0530

    dsa day1

---
+++ lecture 1/area of square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int side,area;
+    cout<<"Enter the side: ";
+    cin>>side;
+    area=side*side;
+    cout<<"Area of the square: "<<area<<endl;
+    return 0;
+}
