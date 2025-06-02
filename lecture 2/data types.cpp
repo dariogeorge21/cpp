a7585552320492cc94f3bf060c49c757b4ee92e0
Dario George <edu.dariogeorge21@gmail.com>
2025-06-02 16:39:15 +0530

    lec2

---
+++ lecture 2/data types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num=3;
+    cout<<"Number is: "<<num<<endl;
+    float number=100.99f;
+    cout<<"Floating number is: "<<number<<endl;
+    char ch='A';
+    cout<<"Character is: "<<ch<<endl;
+    bool flag=true;
+    cout<<"Boolean is: "<<flag<<endl;
+    return 0;
+}
