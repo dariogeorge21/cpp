eb1ba4cc8769c40f653af29c1c3b31989d10e5a8
Dario George <edu.dariogeorge21@gmail.com>
2025-06-02 16:41:08 +0530

    lec2

---
+++ lecture 2/type casting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    char ch='A';
+    int character=ch;
+    cout<<"Character ASCII value is: "<<character<<endl;
+    int num=97;
+    char number=num;
+    cout<<"ASCII character is: "<<number<<endl;
+    return 0;
+}
