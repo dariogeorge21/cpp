b58a21d21a753b7beb303c586cb2a02ab093a0ee
Dario George <edu.dariogeorge21@gmail.com>
2025-06-03 11:00:53 +0530

    lec2

---
+++ lecture 2/relational operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a=10,b=20;
+    cout<<"a is: "<<a<<endl;
+    cout<<"b is: "<<b<<endl;
+    cout<<"a == b: "<<(a == b)<<endl; // Equal to
+    cout<<"a != b: "<<(a != b)<<endl; // Not equal to
+    cout<<"a < b: "<<(a < b)<<endl;   // Less than
+    cout<<"a > b: "<<(a > b)<<endl;   // Greater than
+    cout<<"a <= b: "<<(a <= b)<<endl; // Less than or equal to
+    cout<<"a >= b: "<<(a >= b)<<endl; // Greater than or equal to
+    return 0;
+}
