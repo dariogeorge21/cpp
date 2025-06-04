439877b86afc963cc5a342b98027237a6f768750
Dario George <edu.dariogeorge21@gmail.com>
2025-06-04 10:01:20 +0530

    lec3

--- lecture 3/fibbonacci.cpp
+++ lecture 3/fibbonacci.cpp
@@ -6,12 +6,11 @@ int main(){
     cout<<"Enter the limit: ";
     cin>>limit;
     int first=0,second=1,next;
-    cout<<first<<"\t"<<second;
+    cout<<first<<"\t"<<second<<"\t";
     for (int i=0;i<limit;i++){
         next=first+second;
         first=second;
         second=next;
-        cout<<first<<"\t";
         cout<<second<<"\t";
     }
     return 0;
