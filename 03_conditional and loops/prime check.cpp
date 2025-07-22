efd11efcb3cb68888bf225c4b5dbd5599b664a08
Dario George <edu.dariogeorge21@gmail.com>
2025-07-22 13:27:44 +0530

    fix(loops): prime or not

--- 03_conditional and loops/prime check.cpp
+++ 03_conditional and loops/prime check.cpp
@@ -7,7 +7,7 @@ int main(){
     cout<<"Enter a number:";
     cin>>num;
     int flag=1;
-    for (int i = 2; i<num; i++){
+    for (int i = 2; i*i<=num; i++){
         if(num%i==0){
             flag=0;
             break;
