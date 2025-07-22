c8c6d4ab90f0e4af10dd7097a7367d16f4821317
Dario George <edu.dariogeorge21@gmail.com>
2025-07-22 13:21:01 +0530

    feat(loops): prime or not

---
+++ 03_conditional and loops/prime check.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a number:";
+    cin>>num;
+    int flag=1;
+    for (int i = 2; i<num; i++){
+        if(num%i==0){
+            flag=0;
+            break;
+        }
+    }    
+    if(flag==1){
+        cout<<"The number is prime!!"<<endl;
+    }else{
+        cout<<"The number is not prime!!"<<endl;
+    }
+    return 0;
+}
