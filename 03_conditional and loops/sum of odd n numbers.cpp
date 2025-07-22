f7fa5cd9becc880f423f088647384ec19b566be5
Dario George <edu.dariogeorge21@gmail.com>
2025-07-22 13:12:47 +0530

    feat(loops): sum of n odd numbers

--- 03_conditional and loops/sum of odd n numbers.cpp
+++ 03_conditional and loops/sum of odd n numbers.cpp
@@ -6,11 +6,12 @@ int main(){
     int limit;
     cout<<"Enter the limit: ";
     cin>>limit;
-    int sum = 0;
-    for (int i=0; i < limit; i++){
+    int sum = 0, i = 0;
+    while (i<limit){
         if (i%2!=0){
             sum+=i;
         }
+        i++;
     }
     cout<<"The sum of odd numbers is "<<sum<<" upto "<<limit<<endl;
     return 0;
